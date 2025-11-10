bf283a3cc45813f5cdc6215f39b01e4b1c5d9243
Aadarsh Thakur <thakuraadarsh1@gmail.com>
2025-11-10 17:50:25 +0530

    Polyfill useSyncExternalStore without React shim dependency

--- src/lib/useSyncExternalStoreShim.ts
+++ src/lib/useSyncExternalStoreShim.ts
@@ -1,13 +1,68 @@
-import React from 'react'
+import {
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+} from 'react'
 
-const useSyncExternalStore =
-  React?.useSyncExternalStore ??
-  (((subscribe, getSnapshot) => {
-    throw new Error(
-      'React.useSyncExternalStore is unavailable. Ensure the app runs with React 18+.',
-    )
-  }) as typeof React.useSyncExternalStore)
+type StoreRef<T> = { value: T; getSnapshot: () => T }
 
-export { useSyncExternalStore }
+const objectIs: typeof Object.is =
+  typeof Object.is === 'function'
+    ? Object.is
+    : (value, nextValue) =>
+        (value === nextValue && (value !== 0 || 1 / value === 1 / nextValue)) ||
+        (value !== value && nextValue !== nextValue)
+
+function handleStoreChange<T>(
+  instRef: React.MutableRefObject<StoreRef<T>>,
+  setState: Dispatch<SetStateAction<StoreRef<T>>>,
+) {
+  const { getSnapshot } = instRef.current
+  const nextValue = getSnapshot()
+  if (!objectIs(nextValue, instRef.current.value)) {
+    setState({ value: nextValue })
+  }
+}
+
+function subscribeToStore<T>(
+  subscribe: (listener: () => void) => () => void,
+  instRef: React.MutableRefObject<StoreRef<T>>,
+  setState: Dispatch<SetStateAction<StoreRef<T>>>,
+) {
+  return subscribe(() => {
+    handleStoreChange(instRef, setState)
+  })
+}
+
+export function useSyncExternalStore<T>(
+  subscribe: (listener: () => void) => () => void,
+  getSnapshot: () => T,
+): T {
+  const instRef = useRef<StoreRef<T>>({
+    value: getSnapshot(),
+    getSnapshot,
+  })
+  const [state, setState] = useState<StoreRef<T>>(instRef.current)
+  const value = state.value
+
+  useLayoutEffect(() => {
+    instRef.current.value = value
+    instRef.current.getSnapshot = getSnapshot
+  }, [value, getSnapshot])
+
+  useEffect(() => {
+    return subscribeToStore(subscribe, instRef, setState)
+  }, [subscribe])
+
+  useEffect(() => {
+    handleStoreChange(instRef, setState)
+  })
+
+  return value
+}
+
 export default useSyncExternalStore
 
